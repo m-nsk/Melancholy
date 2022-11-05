b33050763f390a4909a7c12f2df054b6e97df478
Graham Troy <gtroy917@gmail.com>
2022-11-05 13:18:47 -0400

    Here is the code I made

---
+++ todays_diary.py
@@ -0,0 +1,13 @@
+"""Insert objective here!!!"""
+
+from datetime import datetime
+def todays_diary(): 
+  while True: 
+    todays_diary = input("Today's diary is")
+    dates = []
+    sum_todays_diary = []
+    today = datetime.now().strftime("%Y-%m-%d")
+    if today not in dates:
+      sum_todays_diary.append(Todays_diary())
+      dates.append(today)
+      print(sum_todays_diary)
