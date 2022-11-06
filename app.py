7b02754b4456b80c3d883a87afac6073e35605a5
Sam <minsk@unc.edu>
2022-11-05 22:44:13 -0400

    commit before import main

--- app.py
+++ app.py
@@ -11,13 +11,24 @@ def index():
 def write():
     if request.method == "POST":
         written = request.form["write"]
-        # try:
-        save_text.save_text(written)
-        return redirect('/')
-        # except:
-        #    return "There was an error."
+        try:
+            save_text.save_text(written)
+            return redirect('/')
+        except:
+            return "There was an error."
     else:
         return render_template("write.html")
+
+@app.route("/upload", methods=["POST", "GET"])
+def upload():
+    if request.method == "POST":
+        upload = request.form["upload"]
+        try:
+            return redirect('/')
+        except:
+            return "There was an error."
+    else:
+        return render_template("upload.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
